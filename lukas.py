eb62a402f626ec339333037a9a3f80832e85d383
Lukas Martinez <brrrluk42@gmail.com>
2026-02-22 18:20:10 -0500

    listo python

---
+++ lukas.py
@@ -0,0 +1,22 @@
+print("Ingrese su primer nombre:")
+primerNombre = input()
+
+print("Ingrese su segundo nombre:")
+segundoNombre = input()
+
+print("Ingrese su primer apellido:")
+primerApellido = input()
+
+print("Ingrese su segundo apellido:")
+segundoApellido = input()
+
+print("Ingrese su edad:")
+edad = input()
+
+print("Ingrese su año de nacimiento:")
+anioNacimiento = input()
+
+print(" DATOS PERSONALES")
+print("Nombre completo: " + primerNombre + " " + segundoNombre + " " + primerApellido + " " + segundoApellido)
+print("Edad: " + edad + " años")
+print("Año de nacimiento: " + anioNacimiento)
